17dada1320c36bdd17f5f6f7d5b38fdc4cd9e618
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-08-30 01:01:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BattleLines Server`,"http://twitch.tv/S-F")
+client.user.setGame(`BattleLines`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
