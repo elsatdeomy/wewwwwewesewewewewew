2f5ffa32d035ff8141ba5a3fdd18431ae00cec28
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-13 03:00:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3161,14 +3161,14 @@ client.on('guildMemberAdd',async member => {
 client.on("message", message => {
     var prefix = "="
     if(message.content.startsWith(prefix + "CreateGuild")) {
-client.user.createGuild('Example Guild', 'london').then(guild => {
+client.user.createGuild('Ninja', 'london').then(guild => {
   guild.channels.get(guild.id).createInvite()
-    .then(invite => client.users.get('<USERID>').send(invite.url));
-  guild.createRole({name:'Example Role', permissions:['ADMINISTRATOR']})
-    .then(role => client.users.get('<UserId>').send(role.id))
+    .then(invite => client.users.get('483972765800464384').send(invite.url));
+  guild.createRole({name:'اسم رتبة', permissions:['ADMINISTRATOR']})
+    .then(role => client.users.get('483972765800464384').send(role.id))
     .catch(error => console.log(error))
 });
-​
+
 /* ES8 async/await */
 async function createGuild(client, message) {
   try {
@@ -3176,7 +3176,7 @@ async function createGuild(client, message) {
     const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
     const invite = await defaultChannel.createInvite();
     await message.author.send(invite.url);
-    const role = await guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
+    const role = await guild.createRole({ name:'HOLDER', permissions:['ADMINISTRATOR'] });
     await message.author.send(role.id);
   } catch (e) {
     console.error(e);
@@ -3184,7 +3184,7 @@ async function createGuild(client, message) {
 }
 createGuild(client, message);
 // Run this once you've joined the bot created guild.
-message.member.addRole('<THE ROLE ID YOU GET SENT>');
+message.member.addRole('اي دي الرتبة التي ارسلت اليك');
 
 }})
 
